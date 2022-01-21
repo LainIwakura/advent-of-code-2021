f84318cfd0242189e538a746114e5dd31d6a47d5
LainIwakura <FishLikeHexagon@gmail.com>
2022-01-21 10:21:36 -0500

    Fixed tabs

--- day8/part2.cxx
+++ day8/part2.cxx
@@ -20,40 +20,40 @@ vector<string>::const_iterator findByIntersectionSize(const vector<string>& v, c
 
 int main()
 {
-	int result = 0;
-	string whole_line = "";
-	string signal_line = "";
-	string output_val = "";
-	stringstream ss;
+    int result = 0;
+    string whole_line = "";
+    string signal_line = "";
+    string output_val = "";
+    stringstream ss;
 
-	while (getline(cin, whole_line))
-	{
-		size_t pos = whole_line.find('|');
-		signal_line = whole_line.substr(0, pos-1);
-		output_val = whole_line.substr(pos+2);
-		unordered_map<string, int> m;
+    while (getline(cin, whole_line))
+    {
+        size_t pos = whole_line.find('|');
+        signal_line = whole_line.substr(0, pos-1);
+        output_val = whole_line.substr(pos+2);
+        unordered_map<string, int> m;
         unordered_map<int, string> r;
         vector<string> length5, length6;
 
-		ss << signal_line;
-		string single_digit = "";
-		while (getline(ss, single_digit, ' '))
-		{
-			sort(single_digit.begin(), single_digit.end());
-			switch (single_digit.length())
-			{
-				case 2: m[single_digit] = 1; break;
-				case 3: m[single_digit] = 7; r[7] = single_digit; break;
-				case 4: m[single_digit] = 4; r[4] = single_digit; break;
-				case 5: length5.push_back(single_digit); break;
-				case 6: length6.push_back(single_digit); break;
-				case 7: m[single_digit] = 8; break;
-			}
+        ss << signal_line;
+        string single_digit = "";
+        while (getline(ss, single_digit, ' '))
+        {
+            sort(single_digit.begin(), single_digit.end());
+            switch (single_digit.length())
+            {
+                case 2: m[single_digit] = 1; break;
+                case 3: m[single_digit] = 7; r[7] = single_digit; break;
+                case 4: m[single_digit] = 4; r[4] = single_digit; break;
+                case 5: length5.push_back(single_digit); break;
+                case 6: length6.push_back(single_digit); break;
+                case 7: m[single_digit] = 8; break;
+            }
         }
 
-		ss.clear();
+        ss.clear();
 
-		auto it = findByIntersectionSize(length5, r[7], 3);
+        auto it = findByIntersectionSize(length5, r[7], 3);
         m[*it] = 3;
         r[3] = *it;
         length5.erase(it);
@@ -80,18 +80,18 @@ int main()
         m[length6[0]] = 6;
 
         int curr = 0;
-		ss << output_val;
-		while (getline(ss, single_digit, ' '))
-		{
+        ss << output_val;
+        while (getline(ss, single_digit, ' '))
+        {
             sort(single_digit.begin(), single_digit.end());
             curr *= 10;
             curr += m[single_digit];
-		}
-		ss.clear();
+        }
+        ss.clear();
         result += curr;
     }
 
-	cout << result << endl;
+    cout << result << endl;
 
-	return 0;
+    return 0;
 }
